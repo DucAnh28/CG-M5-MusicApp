edde7b70f4d5285b65a89e9ee9f2f24fd0c1c9b8
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-10-24 14:23:56 +0700

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { YoutubePlaylistComponent } from './youtube/youtube-playlist/youtube-playlist.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { YoutubePlayerComponent } from './youtube/youtube-player/youtube-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
